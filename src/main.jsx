5c5ce7d929056dbf99f41b35a1c1a04416458fd7
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-02 16:48:40 +0530

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Error from './components/Error'
+import CountryDetail from './components/CountryDetail'
+import App from './App'
+
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path=":country" element={<CountryDetail />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppRouter />
 )
+
+
+// Different Syntax
+
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Contact from './components/Contact'
+// import Home from './components/Home'
+// import Error from './components/Error'
+// import CountryDetail from './components/CountryDetail'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />,
+//       },
+//       {
+//         path: '/:country',
+//         element: <CountryDetail />,
+//       },
+//       {
+//         path: '/contact',
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <RouterProvider router={router} />
+// )
+
+
+
+
